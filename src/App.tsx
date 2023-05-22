bb766eac42bb49e29ef41c14921f556123fc41f1
Far Sal <faridsalmany@gmail.com>
2023-05-22 05:30:54 -0700

    +add more sample of new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Alert, Badge, Button, DataCard, Typography } from "myreact-ui-lib";
+import {
+  Alert,
+  Badge,
+  Button,
+  DataCard,
+  Typography,
+  Toggle,
+  RangeSlider,
+} from "myreact-ui-lib";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [count, setCount] = useState(20);
+  const toggleHandler = () => {
+    console.log("Toggling");
+  };
   return (
-    <>
+    <React.Fragment>
       <Button variant="success">Package Button</Button>
       <Button variant="outline_danger">Package Button</Button>
       <Typography variant="h1">H1 Header</Typography>
@@ -16,7 +26,15 @@ function App() {
         Alert
       </Alert>
       <Badge text="2" variant="danger" />
-    </>
+      <Toggle onToggle={toggleHandler} isChecked={true} variant="success" />
+      <RangeSlider
+        minValue={10}
+        maxValue={50}
+        initialValue={count}
+        selectedValue={setCount}
+      />
+      <div>KreLink</div>
+    </React.Fragment>
   );
 }
 
