410d611d9f647dbf790464ac101f7c7178fcbadf
Far Sal <faridsalmany@gmail.com>
2023-05-22 02:43:27 -0700

    +install & add some DataDisplay sample to project

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button, Typography } from "myreact-ui-lib";
+import { Alert, Badge, Button, DataCard, Typography } from "myreact-ui-lib";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <Button variant="success">Package Button</Button>
+      <Button variant="outline_danger">Package Button</Button>
       <Typography variant="h1">H1 Header</Typography>
+      <Alert title="Alarm" variant="warning">
+        Alert
+      </Alert>
+      <Badge text="2" variant="danger" />
     </>
   );
 }
