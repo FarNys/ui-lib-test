dde9d27bf7021c90fa1df32436e1849bd824ba97
Far Sal <faridsalmany@gmail.com>
2023-05-28 06:50:35 -0700

    refactor position of component +update package

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -10,32 +10,522 @@ import {
   Typography,
   Toggle,
   RangeSlider,
+  OptionType,
+  Container,
+  Card,
+  Tooltip,
+  Loading,
+  SquareLoading,
+  Accordion,
+  Modals,
+  Toast,
+  Carousel,
+  Checkbox,
+  Dropdown,
+  DropdownMulti,
+  InputGroup,
+  Tabs,
 } from "myreact-ui-lib";
 
 function App() {
-  const [count, setCount] = useState(20);
-  const toggleHandler = () => {
-    console.log("Toggling");
+  const [count, setcount] = useState<number>(0);
+  const [modalOpen, setmodalOpen] = useState<boolean>(false);
+  const [isToast, setisToast] = useState<boolean>(false);
+  const [isToastShow, setisToastShow] = useState<boolean>(false);
+  const btnRef = useRef(null);
+  // const [, setDirection] = useDirection();
+
+  // //CHANGE DIRECTION HANDLER
+  // const directionHandler = () => {
+  //   setDirection();
+  // };
+  //MODAL CONTROLLER
+  const openModalStateHandler = () => {
+    setmodalOpen(true);
   };
+
+  //DROPDOWN SELECT VALUE
+  const selectDataHandler = (e: OptionType): void => {
+    console.log(e);
+  };
+  //DROPDWON MULTI SELECT HANDLER
+  const selectMultiHandler = (e: OptionType[]): void => {
+    console.log(e);
+  };
+
+  //TOGGLE HANDLER FOR CHECKBOX
+  const changeToggleHandler = (e: boolean) => {
+    console.log(e);
+  };
+
+  //CHECKBOX HANDLER TO CONTROL STATE IN PARENT
+  const changeCheckboxHandler = (e: boolean) => {
+    console.log(e);
+  };
+
+  //HANDLER TO GET RANGE SLIDER SELECTED VALUE
+  const selectedValueHandler = (e: number) => {
+    console.log(e);
+  };
+
   return (
-    <React.Fragment>
-      <Button variant="success">Package Button</Button>
-      <Button variant="outline_danger">Package Button</Button>
-      <Typography variant="h1">H1 Header</Typography>
-      <Alert title="Alarm" variant="warning">
-        Alert
-      </Alert>
-      <Badge text="2" variant="danger" />
-      <Toggle onToggle={toggleHandler} isChecked={true} variant="success" />
-      <RangeSlider
-        minValue={10}
-        maxValue={50}
-        initialValue={count}
-        selectedValue={setCount}
+    <>
+      <Container>
+        <Typography style={{ marginLeft: "4.5rem" }} variant="h2">
+          Actions
+        </Typography>
+        <Card>
+          <Typography variant="h4">Buttons</Typography>
+          <Button
+            title="Small"
+            data-testid="increment"
+            role="button"
+            variant="default"
+            size="sm"
+            // onClick={():void => setcount((prev) => prev + 1)}
+            onClick={(): void => setcount((prev) => prev + 1)}
+          />
+          <Button
+            title="Medium"
+            role="button"
+            data-testid="decrement"
+            variant="default"
+            size="md"
+            onClick={(): void => setcount((prev) => prev - 1)}
+            ref={btnRef}
+          />
+          <Button variant="default" size="lg">
+            Large
+          </Button>
+          <Button size="sm" variant="default">
+            Default
+          </Button>
+          <Button size="sm" variant="success">
+            success
+          </Button>
+          <Button size="sm" variant="danger">
+            danger
+          </Button>
+          <Button size="sm" variant="warning">
+            warning
+          </Button>
+          <Button size="sm" variant="outline_default">
+            outline_Default
+          </Button>
+          <Button size="sm" variant="outline_success">
+            outline_success
+          </Button>
+          <Button size="sm" variant="outline_danger">
+            outline_danger
+          </Button>
+          <Button size="sm" variant="outline_warning">
+            outline_warning
+          </Button>
+          {/* <p data-testid="countValue">{count}</p> */}
+        </Card>
+        <Card>
+          <Typography variant="h4">Dropwdown(Single/Multi)</Typography>
+          <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+            <Dropdown options={optionList} onSelect={selectDataHandler} />
+          </div>
+          <DropdownMulti options={optionList} onSelect={selectMultiHandler} />
+        </Card>
+
+        <Card>
+          <Typography variant="h4">Modal</Typography>
+          <div style={{ marginTop: "1rem" }}></div>
+          <Button onClick={openModalStateHandler} size="md" variant="default">
+            Open Modal
+          </Button>
+        </Card>
+        <Card>
+          <Typography variant="h4">Toast</Typography>
+          <div style={{ marginTop: "1rem" }}></div>
+          <Button size="md" variant="default" onClick={() => setisToast(true)}>
+            Show Toast
+          </Button>
+        </Card>
+      </Container>
+
+      <Container>
+        <Typography style={{ marginLeft: "4.5rem" }} variant="h2">
+          Data Display
+        </Typography>
+        <Card>
+          <Typography variant="h4">Typography</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <Typography variant="h1">Test Typography h1</Typography>
+          <Typography variant="h2">Test Typography h2</Typography>
+          <Typography variant="h3">Test Typography h3</Typography>
+          <Typography variant="h4">Test Typography h4</Typography>
+          <Typography variant="h5">Test Typography h5</Typography>
+          <Typography variant="h6">Test Typography h6</Typography>
+          <Typography variant="p">Test Typography p</Typography>
+          <Typography variant="small">Test Typography Span</Typography>
+        </Card>
+        <Card>
+          <Typography variant="h4">Alert</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <Typography variant="small" className="mt-2 mb-1 ">
+            Success Alert
+          </Typography>
+          <Alert variant="success" title="Your purchase has been confirmed!" />
+          <Typography variant="small" className="mt-2 mb-1">
+            Danger Alert
+          </Typography>
+          <Alert variant="danger" title="Error! Task failed successfully." />
+          <Typography variant="small" className="mt-2 mb-1">
+            Info Alert
+          </Typography>
+          <Alert variant="info" title="New software update available." />
+          <Typography variant="small" className="mt-2 mb-1">
+            Warning Alert
+          </Typography>
+          <Alert variant="warning" title="Warning: Invalid email address!" />
+          <Typography variant="small" className="mt-2 mb-1">
+            Default Alert
+          </Typography>
+          <Alert title="Default: Noting Special" />
+        </Card>
+        <Card>
+          <Typography variant="h4">Text Colors</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <Typography variant="p" colorVariant="default">
+            p element with default Variant
+          </Typography>
+          <Typography variant="p" colorVariant="success">
+            p element with success Variant
+          </Typography>
+          <Typography variant="p" colorVariant="danger">
+            p element with danger Variant
+          </Typography>
+          <Typography variant="p" colorVariant="info">
+            p element with info Variant
+          </Typography>
+          <Typography variant="p" colorVariant="warning">
+            p element with warning Variant
+          </Typography>
+        </Card>
+        <Card>
+          <Typography variant="h4">Badge</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <Badge text="default" />
+          <Badge text="success" variant="success" />
+          <Badge text="danger" variant="danger" />
+          <Badge text="info" variant="info" />
+          <Badge text="warning" variant="warning" />
+        </Card>
+        <Card>
+          <Typography variant="h4">Tooltip</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <Tooltip label="tooltip" position="bottom">
+            <p className="mx-4">Tooltip Bottom</p>
+          </Tooltip>
+          <Tooltip label="tooltip" position="top">
+            <p>Tooltip Top</p>
+          </Tooltip>
+        </Card>
+        <Card>
+          <Typography variant="h4">Loadings</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <Loading />
+          <SquareLoading />
+        </Card>
+        <Card>
+          <Typography variant="h4">Content Card</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <div style={{ width: "320px" }}>
+            <DataCard
+              content="You can also use variant modifiers to target media queries like responsive breakpoints, dark mode, prefers-reduced-motion, and more."
+              title="Title"
+              imageUrl="https://www.immune-image.eu/wp-content/uploads/2020/01/publications-immune-image.jpg"
+            />
+          </div>
+        </Card>
+      </Container>
+      <Container>
+        <Typography style={{ marginLeft: "4.5rem" }} variant="h2">
+          Navigation
+        </Typography>
+        <Card>
+          <Typography variant="h4">Accordion(Sidebar)</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <Accordion data={accordionData} />
+        </Card>
+        <Card>
+          <Typography variant="h4">Tabs</Typography>
+          <div style={{ marginBottom: "1rem" }}></div>
+          <Tabs tabsValue={tabsItem} variant="info" />
+        </Card>
+      </Container>
+      <Container>
+        <Typography style={{ marginLeft: "4.5rem" }} variant="h2">
+          Form Display
+        </Typography>
+        <Card>
+          <Typography variant="h4">InputGroup</Typography>{" "}
+          <div style={{ marginTop: "1rem" }}></div>
+          <InputGroup label="Title" name="input-name" />
+          <InputGroup label="Username" name="input-name-2" />
+        </Card>
+        <Card>
+          <Typography variant="h4">Checkbox</Typography>{" "}
+          <div style={{ marginTop: "1rem", display: "flex" }}>
+            <Toggle
+              onToggle={changeToggleHandler}
+              isChecked={true}
+              variant="default"
+            />
+            <Toggle
+              onToggle={changeToggleHandler}
+              isChecked={true}
+              variant="danger"
+            />
+            <Toggle
+              onToggle={changeToggleHandler}
+              isChecked={true}
+              variant="info"
+            />
+            <Toggle
+              onToggle={changeToggleHandler}
+              isChecked={true}
+              variant="warning"
+            />
+            <Toggle
+              onToggle={changeToggleHandler}
+              isChecked={true}
+              variant="success"
+            />
+          </div>
+        </Card>
+        <Card>
+          <Typography variant="h4">Radio</Typography>{" "}
+          <div style={{ marginTop: "1rem", display: "flex" }}>
+            <Checkbox
+              onToggle={changeCheckboxHandler}
+              isChecked={true}
+              variant="default"
+            />
+            <Checkbox
+              onToggle={changeCheckboxHandler}
+              isChecked={true}
+              variant="danger"
+            />
+            <Checkbox
+              onToggle={changeCheckboxHandler}
+              isChecked={true}
+              variant="info"
+            />
+            <Checkbox
+              onToggle={changeCheckboxHandler}
+              isChecked={true}
+              variant="warning"
+            />
+            <Checkbox
+              onToggle={changeCheckboxHandler}
+              isChecked={true}
+              variant="success"
+            />
+          </div>
+        </Card>
+        <Card>
+          <Typography variant="h4">Range Slider</Typography>{" "}
+          <div style={{ marginTop: "1rem", display: "flex" }}>
+            <RangeSlider
+              minValue={20}
+              maxValue={352}
+              selectedValue={selectedValueHandler}
+            />
+          </div>
+        </Card>
+      </Container>
+
+      {/* <Card>
+        This is <LinkText to="/todo">Link</LinkText> Example
+      </Card> */}
+
+      {/* <Card>
+        <Checkbox
+          onToggle={changeCheckboxHandler}
+          isChecked={true}
+          variant="warning"
+        />
+      </Card> */}
+      {/* <Card>
+        <Button
+          // onClick={() => setshowToastPortal(true)}
+          onClick={() =>
+            toastCreator("<div><h1>H1 IS HERE</h1></div>", "success")
+          }
+          size="sm"
+          variant="default"
+          title="Portal Toast"
+        />
+      </Card> */}
+
+      {/* <Card>
+        <Carousel carouselData={carouselData} />
+      </Card> */}
+
+      <Toast
+        toastText="Toast Info Theme"
+        showToast={isToast}
+        setshowToast={setisToast}
+        variant="info"
       />
-      <div>KreLink</div>
-    </React.Fragment>
+      <Toast
+        toastText="Toast Danger Theme"
+        showToast={isToastShow}
+        setshowToast={setisToastShow}
+        variant="danger"
+      />
+      {/* <ToastPortal
+        toastText="This is For Test"
+        showToastPortal={showToastPortal}
+        setshowToastPortal={setshowToastPortal}
+      /> */}
+      <Modals setisOpren={setmodalOpen} isOpen={modalOpen} title="Modal Header">
+        <Typography variant="p">Do you want to exit?</Typography>
+        <div className="flex w-full justify-end">
+          <Button size="sm" variant="info" onClick={() => setmodalOpen(false)}>
+            Yes
+          </Button>
+          <Button
+            size="sm"
+            variant="danger"
+            onClick={() => setmodalOpen(false)}
+          >
+            No
+          </Button>
+        </div>
+      </Modals>
+    </>
   );
 }
 
 export default App;
+
+const tabsItem = [
+  {
+    title: "TAB-1",
+    id: "1",
+    content: (
+      <div>
+        <h6>THIS IS First Item</h6>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          voluptate ducimus numquam perferendis fuga minus id molestiae
+          quibusdam sapiente quam.
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "TAB-2",
+    id: "2",
+    content: (
+      <div>
+        <h6>This Is Second Item</h6>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          voluptate ducimus numquam perferendis fuga minus id molestiae
+          quibusdam sapiente quam.
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "TAB-3",
+    id: "3",
+    content: (
+      <div>
+        <h6>This Is Third Item</h6>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          voluptate ducimus numquam perferendis fuga minus id molestiae
+          quibusdam sapiente quam.
+        </div>
+      </div>
+    ),
+  },
+];
+
+const optionList = [
+  {
+    label: "L-One",
+    value: "Label One",
+  },
+  {
+    label: "L-Two",
+    value: "Label Two",
+  },
+  {
+    label: "Mixer",
+    value: "Label 3",
+  },
+  {
+    label: "TaBLING",
+    value: "Label 4",
+  },
+  {
+    label: "Revol",
+    value: "Label 5",
+  },
+  {
+    label: "Kickso",
+    value: "Label 6",
+  },
+  {
+    label: "VeloNe",
+    value: "Label 7",
+  },
+  {
+    label: "Brand",
+    value: "Label 8",
+  },
+  {
+    label: "Brand-9",
+    value: "Label 9",
+  },
+  {
+    label: "Brand-10",
+    value: "Label 10",
+  },
+  {
+    label: "Brand-11",
+    value: "Label 11",
+  },
+];
+
+const accordionData = [
+  {
+    title: "Parent-1",
+    children: [
+      {
+        title: "childrensSz-1",
+      },
+      {
+        title: "child-2",
+      },
+    ],
+  },
+  {
+    title: "Parent-2",
+    children: [
+      {
+        title: "child-2-1",
+      },
+    ],
+  },
+  {
+    title: "Parent-3",
+    children: [
+      {
+        title: "child-3-1",
+      },
+      {
+        title: "child-3-2",
+      },
+    ],
+  },
+];
--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,7 @@ export default defineConfig({
       allow: [".."],
     },
   },
+  preview: {
+    port: 5000,
+  },
 });
